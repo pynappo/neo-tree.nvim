2b7b253427fb24f30c6d397bc2fe06fda1c2d121
pynappo <pynappo@proton.me>
2025-05-06 16:00:54 -0700

    fixup

--- lua/neo-tree/lib/path/posix.lua
+++ lua/neo-tree/lib/path/posix.lua
@@ -1,7 +1,7 @@
 --- A pathlib-like class suited for neo-tree's purposes.
 local utils = require("neo-tree.utils")
 
----@enum neotree.Path.SpecialType
+---@enum neotree.Path.Type
 local pathtype = {
   WINDOWS_FILE = 0,
   WINDOWS_UNC = 1,
@@ -16,13 +16,17 @@ local pathtype = {
 ---@class neotree.Path
 ---@field [integer] string?
 ---@field root string
----@field private dirty boolean
 ---@field sep string
+---@field type neotree.Path.Type
+---@field private dirty boolean
 ---@field private unnormalized (neotree.Pathish)[]
 ---
 ---@field private _cache neotree.Path.Cache
 local Path = {}
 
+---The default, fastest constructor for creating a path
+---@vararg neotree.Pathish
+---@return neotree.Path new_path
 function Path:new(...)
   local o = {}
   setmetatable(o, self)
@@ -40,7 +44,7 @@ end
 function Path:__div(b)
   if getmetatable(b) == Path then
     ---@cast b neotree.Path
-    return Path:new(unpack(b:normalize()))
+    return Path:new(b)
   end
   if type(b) == "string" then
     return Path:new(tostring(self), b)
@@ -57,19 +61,19 @@ end
 
 ---@param str string
 ---@return string? prefix The prefix of the absolute path, if the path is absolute.
----@return neotree.Path.SpecialType? alt If the prefix exists, whether the prefix is for non-regular paths
+---@return neotree.Path.Type? alt If the prefix exists, the type of the path given
 local extract_prefix = function(str)
   if utils.is_windows then
     local disk_prefix = str:match([=[^%a:[\\/]]=])
     if disk_prefix then
-      return disk_prefix, nil
+      return disk_prefix, pathtype.WINDOWS_FILE
     end
     local unc_prefix = str:match([[\\]])
     if unc_prefix then
       return unc_prefix, pathtype.WINDOWS_UNC
     end
   else
-    return str:match("^/"), nil
+    return str:match("^/"), pathtype.LINUX_FILE
   end
   return nil, nil
 end
@@ -106,23 +110,57 @@ function Path:normalize()
   return self
 end
 
----@param str
----@param relative boolean? Whether the str is known to be relative
-function Path:_append_single_segment(str, relative)
-  local prefix, parts = extract_prefix(str)
-  if prefix then
-    self.prefix = str
+---@param str string
+---@param relative boolean? Whether the str is known to be relative to this path or not
+---@param fast boolean? Whether we should do some slow operations like normalizing path separators or not
+function Path:_append_string(str, relative, fast)
+  local prefix, rest
+  if relative == nil then
+    prefix = extract_prefix(str)
+    relative = prefix ~= nil
+  end
+
+  if utils.is_windows and not fast then
+    str = str:gsub("/", "\\")
+  end
+
+  local segment_iter = vim.gsplit(str, utils.is_windows and "\\" or "/", { plain = true })
+  if relative then
+    for segment in segment_iter do
+      if segment == ".." then
+        self[#self] = nil
+      elseif segment == "." then
+        -- do nothing
+      else
+        self[#self + 1] = segment
+      end
+    end
+  else
+    self:clear()
+    for segment in segment_iter do
+      self[#self + 1] = segment
+    end
+  end
+end
+
+function Path:clear()
+  self.prefix = nil
+  local count = #self
+  for i = 1, count do
+    self[i] = nil
   end
 end
 
 local fast_list_extend
 if table.move then
-  ---@param dst any[]
+  ---@generic T: table
+  ---@param dst T List which will be modified and appended to
   ---@param src any[]
   ---@param start integer?
   ---@param finish integer?
+  ---@return T
   fast_list_extend = function(dst, src, start, finish)
-    table.move(src, start or 1, finish or #src, #dst, dst)
+    return table.move(src, start or 1, finish or #src, #dst, dst)
   end
 else
   fast_list_extend = vim.list_extend
@@ -142,7 +180,7 @@ function Path:parent()
 end
 
 ---@param levels_up integer? How many levels up to go, default 1 level up
----@return Path
+---@return neotree.Path
 function Path:parent_path(levels_up)
   self:normalize()
   return Path:new(fast_list_extend({}, self, 0, #self - (levels_up or 1)))

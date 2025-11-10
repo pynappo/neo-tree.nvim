c66620bcd308b72d89657c73b2caf76563288356
pynappo <pynappo@proton.me>
2025-11-10 12:57:32 -0800

    rename

--- lua/neo-tree/ui/tree.lua
+++ lua/neo-tree/ui/tree.lua
@@ -4,82 +4,90 @@ local api = vim.api
 ---@field [integer] neotree.ui.TreeNode?
 local TreeNode = {}
 local ns = api.nvim_create_namespace("neo-tree.ui.tree")
+local log = require("neo-tree.log")
 
 local next_id = 0
 
+local nodes = {}
 function TreeNode:new(id)
   local o = {}
   setmetatable(o, {
     __index = self,
-    __tostring = self.str,
+    __tostring = function()
+      return self.str(o)
+    end,
   })
   self.id = id or next_id
   self.text = tostring(self.id)
   next_id = next_id + 1
 
-  self.extmarks = {}
+  self.extmarks_for_buf = {}
+  nodes[#nodes + 1] = o
   return o
 end
 
 ---@param self neotree.ui.TreeNode
 ---@return string str
 function TreeNode.str(self)
-  return tostring(self.text)
+  return self.text
 end
 
 ---@param buf integer
-function TreeNode:render(buf, start_line)
-  local nodes = {
-    self,
-    unpack(self),
-  }
-end
----@param line integer
+function TreeNode:render(buf, start_line) end
+
+---@param start_row integer 0-indexed
 ---@param buf integer?
-function TreeNode:draw_at(line, buf, extmark_update)
+---@param end_row integer? 0-indexed
+function TreeNode:draw_at(start_row, buf, end_row)
+  end_row = end_row or start_row
   buf = buf or 0
   local str = self:str()
-  api.nvim_buf_set_lines(buf, line, line, true, { str })
-  self.extmark_id = api.nvim_buf_set_extmark(buf, ns, line, 0, {
-    end_col = #str,
+  api.nvim_buf_set_lines(buf, start_row, end_row, true, { str })
+  local extmark_id = api.nvim_buf_set_extmark(buf, ns, start_row, 0, {
     hl_group = "WarningMsg",
+    invalidate = true,
+    virt_text = { {
+      self.id,
+      "ErrorMsg",
+    } },
+    virt_text_pos = "eol_right_align",
   })
-end
-
-function TreeNode:find_extmark(buf)
-  buf = buf or 0
-  local current_extmark_details = api.nvim_buf_get_extmark_by_id(buf, ns, self.extmark_id, {
-    details = true,
-  })
-  return next(current_extmark_details) and current_extmark_details or nil
-end
-
-function TreeNode:find_line(buf)
-  buf = buf or 0
-  local extmark_details = self:find_extmark(buf)
-  return extmark_details and extmark_details[1]
+  local extmarks_for_buf = self.extmarks_for_buf[buf] or {}
+  extmarks_for_buf[#extmarks_for_buf + 1] = extmark_id
+  self.extmarks_for_buf[buf] = extmarks_for_buf
 end
 
 local dmp = require("neo-tree.utils.diff-match-patch")
 local empty = {}
+
 ---@param buf integer
 ---@param new_text string
 function TreeNode:update_text(buf, new_text)
-  local lnum = self:find_line(buf)
-  assert(lnum)
-  local curline = api.nvim_buf_get_lines(buf, lnum - 1, lnum, false)
-  local diffs = dmp.diff_main(curline[1], new_text)
-  local cursor = 0
-  for _, diff in ipairs(diffs) do
-    local len_of_diff = #diff[2]
-    if diff[1] == dmp.DIFF_DELETE then
-      api.nvim_buf_set_text(buf, lnum - 1, cursor, lnum - 1, cursor + len_of_diff, empty)
-    elseif diff[2] == dmp.DIFF_ADD then
-      api.nvim_buf_set_text(buf, lnum - 1, cursor, lnum - 1, cursor, { diff[2] })
-      cursor = cursor + len_of_diff
-    else
-      cursor = cursor + len_of_diff
+  for _, extmark_id in ipairs(self.extmarks_for_buf[buf] or {}) do
+    local extmark_details = vim.api.nvim_buf_get_extmark_by_id(buf, ns, extmark_id, {})
+    local start_row = extmark_details[1]
+    if extmark_details[3].invalid then
+      log.error("invalidated")
+      vim.api.nvim_buf_del_extmark(buf, ns, extmark_id)
+      return
+    end
+
+    local curline = api.nvim_buf_get_lines(buf, start_row, start_row + 1, true)
+    local diffs = dmp.diff_main(curline[1], new_text)
+
+    local cursor = 0
+    for _, diff in ipairs(diffs) do
+      local len_of_diff = #diff[2]
+      if diff[1] == dmp.DIFF_DELETE then
+        api.nvim_buf_set_text(buf, start_row, cursor, start_row, cursor + len_of_diff, empty)
+      elseif diff[1] == dmp.DIFF_INSERT then
+        api.nvim_buf_set_text(buf, start_row, cursor, start_row, cursor, { diff[2] })
+        cursor = cursor + len_of_diff
+      else
+        cursor = cursor + len_of_diff
+      end
     end
   end
 end
+
 return TreeNode
